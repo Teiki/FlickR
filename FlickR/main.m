d52e776ac0c3f22b81570d7597f44451b0e21069
Antoine Galtier <antoinegaltier@MacBook-Pro-de-Antoine.local>
2014-04-10 09:56:19 +0200

    Initial Commit

---
+++ FlickR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlickR
+//
+//  Created by Antoine Galtier on 10/04/2014.
+//  Copyright (c) 2014 Antoine Galtier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
